d3aa73d6216e8143a2256750c96ab647437ac6a6
StoyanMalinin <stoyanmalinin2002@gmail.com>
2022-06-17 00:10:03 +0300

    bug fix

--- src/GameLogic/Entities/Player/WarriorPlayer.cpp
+++ src/GameLogic/Entities/Player/WarriorPlayer.cpp
@@ -1,5 +1,5 @@
 #include "WarriorPlayer.h"
 
 WarriorPlayer::WarriorPlayer(const String& name, int r, int c, const FightController& fc, const ItemManagerController& imc, const MoveController& mc, const PointsDistributionController& pdc, ItemExchangeMaster& iem, FightMaster& fm)
-    : Player(name, r, c, 10, 40, 50, fc, imc, mc, pdc, iem, fm)
+    : Player(name, r, c, 40, 10, 50, fc, imc, mc, pdc, iem, fm)
 {}
